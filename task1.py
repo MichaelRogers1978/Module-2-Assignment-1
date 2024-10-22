cf462c4ae8e6b4ed79f44857e78234e59bf6e80f
MichaelRogers1978 <mikeangie3049@yahoo.com>
2024-10-21 17:04:43 -0700

    first commit

---
+++ task1.py
@@ -0,0 +1,45 @@
+#this is mod2 assignment1 task1.1
+
+
+weather = "sunny"
+if weather == "sunny":
+    print("Wear Sunglasses!")
+else:
+    print("Take an Umbrella!")
+
+
+
+#task1.2
+
+
+user = "How are you doing today?"
+if user == "Happy" :
+    print("That's great, happy to hear!")
+else:
+    print("I hope your day gets better!")
+
+
+#task 2.1
+
+PI = 3.14
+user_Age = 25
+user_location = "New York"
+MAX_LIMIT: 1000
+
+
+#task 3.1
+
+bread = 5
+peanutbutter = 4
+jelly = 6
+sandwich = (bread + peanutbutter + jelly)
+print(sandwich)
+
+
+#task3.2
+bank_account = 5000
+interest_rate = .7
+total_accout_interest = bank_account * interest_rate
+print(total_accout_interest)
+
+
